de22c93c8567a406760747479d18ab284ea03a69
Jordan Blakey <454820+jordanblakey@users.noreply.github.com>
2025-05-24 22:00:14 -0500

    tests passing

---
+++ tests/test_ui.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestUi(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_placeholder(self):
+        self.assertTrue(True, "Placeholder test")
