f2568730467e3da4e614196c89fa39157440942b
Jordan Blakey <454820+jordanblakey@users.noreply.github.com>
2025-06-14 20:19:39 -0500

    open a new window, split console & text editor & info panel. paste code into text editor and run script

--- experiments/31_blend_file_teardown/main2.py
+++ experiments/31_blend_file_teardown/main2.py
@@ -1,45 +1,48 @@
 import bpy
 
-# bpy.types.Workspace.bl_rna.properties["type"].enum_items.keys()
 # bpy.types.Area.bl_rna.properties["type"].enum_items.keys()
 
-# Remove all workspaces except the current one
-for ws in bpy.data.workspaces:
-    if ws != bpy.context.workspace:
-        bpy.data.batch_remove(ids=(ws,))
 
-# ws = bpy.data.workspaces[0]
-# print(ws)
+def main():
+    # Remove all workspaces except the current one
+    for ws in bpy.data.workspaces:
+        if ws != bpy.context.workspace:
+            bpy.data.batch_remove(ids=(ws,))
 
-bpy.ops.wm.window_new()
-print(bpy.context.window.screen)
-print(bpy.context.window.screen.areas)
-for area in bpy.context.window.screen.areas:
-    print(area.type)
-    with bpy.context.temp_override(window=bpy.context.window, area=area):
-        area.type = "CONSOLE"
-        for _ in range(5):
-            bpy.ops.wm.context_cycle_int(
-                data_path="space_data.font_size", reverse=False
+    bpy.ops.wm.window_new()
+    for area in bpy.context.window.screen.areas:
+        with bpy.context.temp_override(window=bpy.context.window, area=area):
+            # open a console and make the font big
+            area.type = "CONSOLE"
+            increase_font_size(8)
+
+            bpy.ops.screen.area_split(direction="VERTICAL", factor=0.5)
+            # split the area and change the type to info
+            area.type = "INFO"
+
+            # split the area and change the type to text e
+            bpy.ops.screen.area_split(direction="HORIZONTAL", factor=0.3)
+            area.type = "TEXT_EDITOR"
+
+            increase_font_size(8)
+
+            # note that we can look for existing text blocks in the blender file
+            bpy.data.texts.new(name="test_text")
+            # and set the text block to the current text editor
+            area.spaces.active.text = bpy.data.texts["test_text"]
+            # set the text editor to a specific text block
+            bpy.ops.text.insert(
+                text="print('Hello, World! This is a test text block.')"
             )
-        print("checking for existing text blocks")
-        print(bpy.data.texts)
-        bpy.ops.screen.area_split(direction="VERTICAL", factor=0.5)
-        area.type = "TEXT_EDITOR"
-        print(type(area))
-        bpy.types.Area.bl_rna.properties["type"].enum_items.keys()
-        # if area.type == 'VIEW_3D':
-        #     bpy.ops.screen.area_close()
-        # elif area.type == 'TEXT_EDITOR':
-        #     bpy.ops.screen.area_close()
-        # elif area.type == 'PROPERTIES':
-        #     bpy.ops.screen.area_close()
-        # elif area.type == 'OUTLINER':
-        #     bpy.ops.screen.area_close()
-        # elif area.type == 'NODE_EDITOR':
-        #     bpy.ops.screen.area_close()
+            bpy.ops.text.run_script()
 
-# bpy.ops.wm.save_mainfile(filepath="test.blend", check_existing=False)
-# sleep(10)
-# bpy.ops.wm.quit_blender()
-#
+
+def increase_font_size(n):
+    for _ in range(n):
+        bpy.ops.wm.context_cycle_int(
+            data_path="space_data.font_size", reverse=False
+        )
+
+
+if __name__ == "__main__":
+    main()
